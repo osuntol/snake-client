5376208cef1c23614804062325012c5b2d3173b4
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-19 04:03:54 +0000

    play file

--- play.js
+++ play.js
@@ -1,27 +1,11 @@
 
-const { stdin } = require('process');
 const {connect} = require('./client.js')
+const { setupInput } = require("./input.js")
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function (key) {
-  console.log('key:',key)
-  if (key === '\u0003'){
-    process.exit()
-  }
-};
+
 
 console.log('connecting....')
-connect();
-setupInput();
+let connectVar = connect();
+setupInput(connectVar);
 
-module.exports = {
-  setupInput
-}
