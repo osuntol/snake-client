4008353c153d2104197062cd735774edc8bef47e
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-19 04:16:45 +0000

    clients with constants

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
+const { IP, PORT } = require("./constants");
 
 const net = require("net")
 
 const connect = function() {
 
   const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
+    IP,
+    PORT 
   })
 
   conn.on('connect', () => {
