0b6a88c22d047b1050c0150ec6e8a8d51fb9468b
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-19 04:04:27 +0000

    input file

---
+++ input.js
@@ -0,0 +1,39 @@
+const { connect } = require("http2");
+
+let connection 
+
+
+
+
+
+const setupInput = function (conn) {
+  connection = conn
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  console.log('key:',key)
+  if (key === '\u0003'){
+    process.exit()
+  } if (key === 'w'){
+    connection.write('Move: up');
+  } 
+   if (key === 'a'){
+    connection.write('Move: left');
+  } 
+   if (key === 'd'){
+    connection.write('Move: right');
+  } 
+   if (key === 's'){
+    connection.write('Move: down');
+  } 
+}
+
+module.exports = {
+  setupInput
+}
